f94b7816210a0f9f322b5697dab4a247182bc274
anabarbieri <134944394+anabarbieri@users.noreply.github.com>
2024-05-08 10:31:11 -0300

    Add files via upload

---
+++ ColaPrioridadTDA.java
@@ -0,0 +1,10 @@
+package codigo;
+
+public interface ColaPrioridadTDA {
+	void inicializarCola();                //sin precondiciones
+	void acolarPrioridad(int x, int y);    //cola inicializada
+	void Desacolar();                      //cola inicializada y no vacia
+	boolean colaVacia();                   //cola inicializada
+	int Primero();                         //cola inicializada y no vacia
+	int Prioridad();                       //cola inicializada y no vacia
+}
