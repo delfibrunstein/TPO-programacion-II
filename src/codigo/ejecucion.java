34c157a095bcb6b9afeba6f79a5aca298fed7d1e
anabarbieri <134944394+anabarbieri@users.noreply.github.com>
2024-05-17 18:37:31 -0300

    Update ejecucion.java

--- src/codigo/ejecucion.java
+++ src/codigo/ejecucion.java
@@ -5,8 +5,8 @@ public class ejecucion {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// Se inicializa la cola de prioridad y los conjuntos determinados 
-		ColaPrioridad cola = new ColaPrioridad();
+	// Se inicializa la cola de prioridad y los conjuntos determinados 
+	ColaPrioridad cola = new ColaPrioridad();
         cola.inicializarCola();
 
         Conjunto ConjuntoEmpresa = new Conjunto();
@@ -19,7 +19,7 @@ public class ejecucion {
         ConjuntoParticularNoCliente.inicializarConjunto();
 
         // Elementos de entrada que representan diferentes tipos de clientes en la cola de prioridad
-        int[] elementos = {1, 2, 3, 3, 1, 2, 2, 1, 3, 1, 2, 1, 3, 1, 2};
+        int[] elementos = {1, 3, 1, 2, 1, 2, 1, 3, 3, 3, 2, 1, 3, 2, 1, 2, 3, 3, 2, 2, 1, 1};
 
         // Se acolan los elementos segun su tipo de cliente
         for (int i = 1; i < elementos.length; i++) {   
