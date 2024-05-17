b65c6534cd4a5fe7e600faf89f3be8ea4917892e
anabarbieri <134944394+anabarbieri@users.noreply.github.com>
2024-05-17 18:35:06 -0300

    Update ColaPrioridad.java

--- src/codigo/ColaPrioridad.java
+++ src/codigo/ColaPrioridad.java
@@ -8,19 +8,21 @@ public class ColaPrioridad {
 	}
 	Elemento [] elementos;
 	int indice;
-	
+
+	// Inicializa la cola de prioridad con un array de tamaño 100
 	public void inicializarCola() {
 		indice = 0;
 		elementos = new Elemento [100];
 	}
-	
+
+	// Agrega un elemento con una prioridad específica a la cola
 	public void acolarPrioridad(int x, int prioridad) {
 		int j = indice;
 		//desplaza a la derecha los elementos de la cola mientras
 		//estos tengan mayor o igual prioridad que la de x
 		for (; j > 0 && elementos[j - 1].prioridad >= prioridad; j--) {
-		//mientras j sea mayor que 0 y la prioridad del elemento en la posición j - 1 
-		//sea mayor o igual que la prioridad del nuevo elemento
+			//mientras j sea mayor que 0 y la prioridad del elemento en la posición j - 1 
+			//sea mayor o igual que la prioridad del nuevo elemento
 			elementos[j] = elementos[j - 1];
 		}
 		elementos[j] = new Elemento();
@@ -28,21 +30,25 @@ public class ColaPrioridad {
 		elementos[j].prioridad = prioridad;
 		indice ++;
 	}
-	
+
+	// Elimina el elemento con la mayor prioridad (al final del array)
 	public void Desacolar() {
 		elementos[indice - 1] = null;
 		indice --;
 		
 	}
-	
+
+	// Verifica si la cola está vacía
 	public boolean colaVacia() {
 		return (indice == 0);
 	}
-	
+
+	// Devuelve el valor del elemento con la mayor prioridad
 	public int Primero() {
 		return elementos[indice - 1].valor;
 	}
-	
+
+	// Devuelve la prioridad del elemento con la mayor prioridad
 	public int Prioridad() {
 		return elementos[indice - 1].prioridad;
 	}
